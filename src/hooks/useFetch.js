44db8c84dfab7c6e738be5d93c09c7cfa3c96757
harshahossain <harshahossain@gmail.com>
2023-12-01 14:48:02 +0600

    Added Custom Hooks
    managing & returning States with custom hooks

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,33 @@
+//custom hooks aare just basically fun where we can def other hooks like useState and stuff
+//must start with use though cause convention
+
+import { useEffect, useState } from "react";
+//import { fetchUserPlaces } from "../http";
+
+export function useFetch(fetchFn, initialValue) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState(initialValue);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch data." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]);
+
+  return {
+    isFetching,
+    fetchedData,
+    error,
+  };
+}
